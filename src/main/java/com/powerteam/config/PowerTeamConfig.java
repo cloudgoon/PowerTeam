c732c156655f5afbb265bdad6cbc91ea30987831
yp <peng.yu@e-melt.com>
2016-09-05 13:56:28 +0800

    init

---
+++ src/main/java/com/powerteam/config/PowerTeamConfig.java
@@ -0,0 +1,35 @@
+package com.powerteam.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+public class PowerTeamConfig {
+
+    /**
+     * 网站的根路径 例如 /powerteam
+     */
+    private String webRoot;
+
+    /**
+     * 网站绝对物理路径 例如 D:/Tomcat/webapps/powerteam/WEB-INF/classes/
+     */
+    private String absoluteWebRoot;
+
+    @Value("${powerTeam.title}")
+    private String title;
+
+    @Value("${powerTeam.pageSize}")
+    private Integer pageSize;
+
+    @Value("${powerTeam.openRegistry}")
+    private Boolean openRegistry;
+
+    @Value("${powerTeam.baiduMapAk}")
+    private String baiduMapAk;
+
+    @Value("${spring.mail.username}")
+    private String mailFrom;
+}
